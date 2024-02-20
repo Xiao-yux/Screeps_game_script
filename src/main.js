1931d4486e31373c60c555e9f4cfef97ef008442
小羽 <1409109991@qq.com>
2024-02-20 13:51:05 +0800

    1

---
+++ src/main.js
@@ -0,0 +1,9 @@
+var roleHarvester = require('./move/role.harvester.js');
+
+module.exports.loop = function () {
+
+    for(var name in Game.creeps) {
+        var creep = Game.creeps[name];
+        roleHarvester.run(creep);
+    }
+}
